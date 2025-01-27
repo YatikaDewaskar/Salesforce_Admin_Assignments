3fb453384dceabeefdacce73e70370303a412501
YatikaDewaskar <yatika.dewaskar@fexle.com>
2025-01-27 18:46:45 +0530

    LWC Assignment 02

---
+++ src/LWC Assignments/LWC - Assignment 02/accountManager/accountManager.js
@@ -0,0 +1,28 @@
+import { LightningElement, wire, track } from 'lwc';
+import getAccounts from '@salesforce/apex/GetAccountsForLWCDataTable.getAccounts';
+
+const columns = [
+    { label: 'Name', fieldName: 'Name' },
+    { label: 'Id', fieldName: 'Id' }
+     
+];
+
+export default class accountManager extends LightningElement {
+    @track columns = columns;
+   // @track data = [];
+    @track accounts;
+
+
+    @wire(getAccounts)
+    wiredAccounts({ error, data })
+    {
+        if(data)
+        {
+            this.accounts = data;
+        }
+        else if(error)
+        {
+            console.log(error);
+        }
+    }
+}
