93e48ff724dfe8c38408765710ebe0eb4ffd5645
YatikaDewaskar <yatika.dewaskar@fexle.com>
2025-01-27 18:58:05 +0530

    LWC Assignment 03

---
+++ src/LWC Assignments/LWC Assignment 03/getAccountDetails/getAccountDetails.js
@@ -0,0 +1,62 @@
+import { LightningElement, api , wire } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import Account from '@salesforce/schema/Account';
+import Contact from '@salesforce/schema/Contact';
+import Name_Field from '@salesforce/schema/Account.Name';
+import Company_FIELD from '@salesforce/schema/Account.Account_Company__c';
+import Website_FIELD from '@salesforce/schema/Account.Website';
+import Phone_FIELD from '@salesforce/schema/Account.Phone';
+import FirstName from '@salesforce/schema/Contact.FirstName';
+import LastName from '@salesforce/schema/Contact.LastName';
+import Phone from '@salesforce/schema/Contact.Phone';
+import Email from '@salesforce/schema/Contact.Email';
+
+export default class GetAccountDetails extends LightningElement {
+   
+    @api recordId;
+    
+    accountObj = Account;
+    contactObj = Contact;
+    Name = Name_Field;
+    company = Company_FIELD;
+    website = Website_FIELD;
+    phone = Phone_FIELD;
+    FirstNamefield = FirstName;
+    LastNamefield = LastName;
+    Emailfield = Email;
+    Phonefield = Phone;
+    iscreateContact = false;
+   
+   /*
+    @wire(getRecord, { recordId: '$recordId', fields: [Name_Field, Company_FIELD,Website_FIELD, Phone_FIELD] })
+    wiredAccount({ error, data }) {
+        if (data) {
+            this.accountData = data;
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+            this.accountData = undefined;
+        }
+    }*/
+
+    handleCreateContact(event){
+       this.iscreateContact = true;
+       console.log("successfully Clicked");
+    }
+
+
+    saveCreateContact(){
+        /*const events = new ShowToastEvent({
+            title: 'Contact Created',
+            message: 'Contact Created Successfully',
+            variant: 'success'
+        });
+        this.dispatchEvent(event);
+*/      console.log("successfully created");
+        this.iscreateContact = false;
+    }
+
+
+   
+}
