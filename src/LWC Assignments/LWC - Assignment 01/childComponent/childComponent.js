60005e0f995ed9d46affb3ca69de3e2f5f949019
YatikaDewaskar <yatika.dewaskar@fexle.com>
2025-01-27 18:28:39 +0530

    LWC - Assignment 01
     parentComponent :
    - displaying message 'Hello World'
    childComponent :
    - Displaying reflected greeting and Adding CSS

---
+++ src/LWC Assignments/LWC - Assignment 01/childComponent/childComponent.js
@@ -0,0 +1,15 @@
+import { LightningElement, api } from 'lwc';
+
+export default class ChildComponent extends LightningElement {
+
+    @api message;
+ //   @api message = 'World Salesforce';
+  
+//    @api
+//    changeMessage(){
+//        this.message = 'Hello World its 2025 inside method...';
+//        return this.message;
+//    }
+
+
+}
