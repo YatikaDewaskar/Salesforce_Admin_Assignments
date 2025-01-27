c95f9d3df988ccc105732a7cc902922f13171521
YatikaDewaskar <yatika.dewaskar@fexle.com>
2025-01-27 18:29:30 +0530

    childComponent.js

--- src/LWC Assignments/LWC - Assignment 01/childComponent/childComponent.js
+++ src/LWC Assignments/LWC - Assignment 01/childComponent/childComponent.js
@@ -3,13 +3,5 @@ import { LightningElement, api } from 'lwc';
 export default class ChildComponent extends LightningElement {
 
     @api message;
- //   @api message = 'World Salesforce';
-  
-//    @api
-//    changeMessage(){
-//        this.message = 'Hello World its 2025 inside method...';
-//        return this.message;
-//    }
-
 
 }
