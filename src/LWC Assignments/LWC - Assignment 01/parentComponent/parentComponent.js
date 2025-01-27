6eab3acde76c81f78c5af239c1eed6b886187338
YatikaDewaskar <yatika.dewaskar@fexle.com>
2025-01-27 18:30:43 +0530

    parentComponent.js

--- src/LWC Assignments/LWC - Assignment 01/parentComponent/parentComponent.js
+++ src/LWC Assignments/LWC - Assignment 01/parentComponent/parentComponent.js
@@ -2,15 +2,6 @@ import { LightningElement } from 'lwc';
 
 export default class ParentComponent extends LightningElement {
 
-    
- //   handleMessage(event){
- //      this.parentVar = this.template.querySelector('c-child-component').message;
- //   }
-
- //   handleMethod(event){
- //       this.parentVar = this.template.querySelector('c-child-component').changeMessage();
- //   }
-
     parentGreetting;
     handleChangeGreetings(event){
         this.parentGreetting = event.target.value;
